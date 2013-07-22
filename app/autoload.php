e52473851683a2e65c00ea7530aeae35ae3d41f9
vitiko <vitiko@mail.ru>
2013-07-22 18:10:32 +0400

    update

--- app/autoload.php
+++ app/autoload.php
@@ -2,6 +2,8 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
+
+
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 // intl
--- src/Iphpsandbox/PhotoBundle/Controller/PhotoController.php
+++ src/Iphpsandbox/PhotoBundle/Controller/PhotoController.php
@@ -15,10 +15,10 @@ class PhotoController extends Controller
         $photo = new Photo();
         $uploadForm = $this->createFormBuilder($photo)
             ->add('title')
-            ->add('date','date')
+            ->add('date', 'date')
 
-            //Using standart field type
-            ->add ('photo','file')
+        //Using standart field type
+            ->add('photo', 'file')
             ->getForm();
 
 
@@ -34,7 +34,41 @@ class PhotoController extends Controller
 
         return $this->render('IphpsandboxPhotoBundle:Photo:index.html.twig', array(
             'uploadForm' => $uploadForm->createView(),
-            'photos' => $em->getRepository ('IphpsandboxPhotoBundle:Photo')->findAll()
+            'photos' => $em->getRepository('IphpsandboxPhotoBundle:Photo')->findAll()
         ));
+    }
+
+
+    public function editAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $photo = $em->getRepository('IphpsandboxPhotoBundle:Photo')->findOneById($id);
+
+        $editForm = $this->createFormBuilder($photo)
+            ->add('title')
+            ->add('date', 'date')
+
+        //Using  field type with showing file/image info
+            ->add('photo', 'iphp_file')
+            ->getForm();
+
+
+        if ($request->isMethod('POST')) {
+            $editForm->bind($request);
+
+            if ($editForm->isValid()) {
+                $em->persist($photo);
+                $em->flush();
+                return $this->redirect($this->generateUrl('iphpsandbox_photo_edit', array ('id' => $photo->getId())));
+            }
+        }
+
+        return $this->render('IphpsandboxPhotoBundle:Photo:edit.html.twig', array(
+
+            'photo' => $photo,
+            'editForm' => $editForm->createView(),
+        ));
+
     }
 }
